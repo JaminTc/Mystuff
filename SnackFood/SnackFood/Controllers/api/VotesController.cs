c8713ec72836eb47a1054b2bd167eca99a78f665
Jamin Tcimpidis <Tcimpidj@csp.edu>
2015-07-29 22:49:25 -0500

    working on some things

--- SnackFood/SnackFood/Controllers/api/VotesController.cs
+++ SnackFood/SnackFood/Controllers/api/VotesController.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using SnackFood.Models;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace SnackFood.Controllers.api
 {
     public class VotesController : ApiController
     {
+        public List<ExistingSnacks> Get()
+        {
+
+            Uri ourUri = new Uri("https://api-snacks.nerderylabs.com/v1/snacks/");
+
+            WebRequest myWebRequest = WebRequest.Create(ourUri);
+
+            myWebRequest.Headers["Authorization"] = "ApiKey fe152ad3-bbef-4452-aad1-baa0cd10532d";
+
+           var theRequest = new StreamReader(myWebRequest.GetResponse().GetResponseStream());
+
+           var body = theRequest.ReadToEnd();
+           List<ExistingSnacks> SnackList = JsonConvert.DeserializeObject<List<ExistingSnacks>>(body);
+            
+            return SnackList;
+           
+        }
+           
     }
 }
---
+++ SnackFood/SnackFood/Models/ExistingSnacks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnackFood.Models
+{
+    public class ExistingSnacks
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string optional { get; set; }
+        public string purchaseLocations { get; set; }
+        public string purchaseCount { get; set; }
+        public string lastPurchaseDate { get; set; }
+    }
+}
